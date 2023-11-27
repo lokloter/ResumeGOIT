a728dad3a8a8728c76fe7621d23f717a0f061642
lokloter <lokloter@gmail.com>
2023-11-27 16:54:55 +0200

    шось знов змінилось

--- script.js
+++
@@ -1,7 +0,0 @@
-const galleryItems = document.querySelectorAll('.gallery-item img');
-
-galleryItems.forEach(item => {
-  item.addEventListener('click', () => {
-    item.classList.toggle('enlarged');
-  });
-});
